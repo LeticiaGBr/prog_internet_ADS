eef98e75a602dda3dd36e7529640eba93e635aaf
Letícia Gehrke Bruda <leticia.gb@aluno.ifsc.edu.br>
2024-12-09 08:49:34 -0300

    listaphp

---
+++ ListaPHP/2.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>2</title>
+</head>
+<body>
+    <!--
+        O PHP é uma linguagem de programação interpretada executada no servidor, 
+        ao contrário do JavaScript, que roda no lado do cliente. 
+        Quando uma requisição HTTP chega no servidor, o PHP processa o código e gera uma resposta em HTML, que enviada é pro navegador do usuário.
+    -->
+</body>
+</html>
